09f664285832efdd91ce0a8b2e1e2849f32dc569
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-20 20:13:24 -0500

    Initial commit

--- be-yielding.js
+++ be-yielding.js
@@ -2,7 +2,7 @@ import {BeBound} from 'be-bound/be-bound.js';
 /** @import {Actions, PAP, AllProps, AP, BAP, Binding} from './ts-refs/be-bound/types.d.ts' */;
 
 class BeYielding extends BeBound{
-    addLocalAbs(localAbsObj, remoteShareObj){}
+    addRemoteAbs(remoteAbsObj, localShareObj){}
 
     /**
      * 
@@ -10,7 +10,7 @@ class BeYielding extends BeBound{
      * @param {Binding} binding 
      * @returns 
      */
-       async reconcileValues(self, binding) {
+    async reconcileValues(self, binding) {
         const { enhancedElement } = self;
         const { localAbsObj, localShareObj, remoteAbsObj, remoteShareObj} = binding;
         const localVal = await localAbsObj.getValue();
